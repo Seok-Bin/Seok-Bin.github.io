64d0281e23b12bbee54d007836f7c06ebff8ac50
SeokBin <89571256+Seok-Bin@users.noreply.github.com>
2021-09-05 19:08:43 +0900

    Update quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -1,43 +1,43 @@
 const quotes = [
   {
-    quote: "Curious things, habits. People themselves never knew they had them.",
-    author: "Agatha Christie",
+    quote: "나이가 성숙을 보장하지는 않는다.",
+    author: "(Lawana Blackwell)",
   },
   {
-    quote: "Recompense injury with justice, and recompense kindness with kindness",
-    author: "Confucius",
+    quote: "여행은 되돌아 보았을 때에만 매력적이다.",
+    author: "(Paul Theroux)",
   },
   {
-    quote: "To love someone is to identify with them",
-    author: "Aristotle",
+    quote: "내가 태어났을 때 나는 너무 놀라서 1년 반동안 말을 할 수 없었다.",
+    author: "(Gracie Allen)",
   },
   {
-    quote: "I never did a day's work in my life. It was all fun.",
-    author: "Thomas A. Edison",
+    quote: "인생은 지긋지긋한 일의 반복이 아니라 지긋지긋한 일의 연속이다.",
+    author: "(Edna St. Vincent Millay)",
   },
   {
-    quote: "I never think of the future - it comes soon enough.",
-    author: "Albert Einstein",
+    quote: "20대에 당신의 얼굴은 자연이 준 것이지만, 50대의 당신의 얼굴은 스스로 가치를 만들어야 한다.",
+    author: "(Gabriel Coco Chanel)",
   },
   {
-    quote: "Each of us visits this Earth involuntarily, and without an invitation. For me, it is enough to wonder at the secrets.",
-    author: "Albert Einstein",
+    quote: "우리는 나이가 들면서 변하는 게 아니다. 보다 자기다워지는 것이다.",
+    author: "(Lynn Hall)",
   },
   {
-    quote: "Keep your face to the sunshine and you cannot see the shadow.",
-    author: "Helen Adams Keller",
+    quote: "행동의 가치는 그 행동을 끝까지 이루는 데 있다.",
+    author: "(Genghis Khan)",
   },
   {
-    quote: "What can you do to promote world peace? Go home and love your family.",
-    author: "Teresa ",
+    quote: "결국 삶이란 여러분이 되고자 했던 완벽한 인격체로 거듭나는 것입니다.",
+    author: "(Oprah Winfrey)",
   },
   {
-    quote: "My life didn't please me, so I created my life.",
-    author: "coco",
+    quote: "성공한 사람이 될 수 있는데 왜 평범한 이에 머무르려 하는가?",
+    author: "(Bertolt Brecht)",
   },
   {
-    quote: "The greatest risk is the risk of riskless living.",
-    author: "Stephen Covey",
+    quote: "인생에서 실패한 사람 중 다수는 성공을 목전에 두고도 모른 채 포기한 이들이다.",
+    author: "(Thomas A. Edison)",
   }
 ];
 
